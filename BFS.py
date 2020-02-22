29614e6b17021dd5bda05fb15743e43e200e5130
Gabriel Oduori <gabriel.oduori@gmail.com>
2020-02-22 23:17:30 +0300

    Added BFS code.

--- BFS.py
+++ BFS.py
@@ -14,6 +14,7 @@ distance) for some or all graph locations.
 Queue: Data structure used by the search algorithm to decide the order in which to process the graph locations.
 """
 import collections
+from implementation import *
 
 class SimpleGraph():
     def __init__(self):
@@ -35,8 +36,25 @@ class Queue:
     
     def get(self):
         return self.elements.popleft()
-        
 
+def breadth_first_search(graph, start):
+    """
+    Implementing BSF and priting out what we find
+    """
+    frontier = Queue()
+    frontier.put(start)
+    visited = {}
+    visited[start] = True
+
+
+    while not frontier.empty():
+        current =  frontier.get()
+        print("Visiting %r" %current)
+        if next not in visited:
+            frontier.put(next)
+            visited[next]= True
+
+breadth_first_search(example_graph, 'A')
 
 example_graph = SimpleGraph()
 example_graph.edges = {
