24bcbd10c3a43ebd95a0101119ddd17f8f6889bf
Gabriel Oduori <gabriel.oduori@gmail.com>
2020-02-22 23:08:08 +0300

    Added Queue class This queue class is a wrapper around the built-in collections.deque class.

--- BFS.py
+++ BFS.py
@@ -23,7 +23,19 @@ class SimpleGraph():
     def neighbors(self, id):
         return self.edges[id]
 
+class Queue:
+    def __init__(self):
+        self.elements = collections.deque()
 
+    def empty(self):
+        return len(self.elements)==0
+    
+    def put(self, x):
+        self.elements.append(x)
+    
+    def get(self):
+        return self.elements.popleft()
+        
 
 
 example_graph = SimpleGraph()
