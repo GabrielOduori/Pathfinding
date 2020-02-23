d7398a2bed780b06197abba1b50bdafc6364f657
Gabriel Oduori <gabriel.oduori@gmail.com>
2020-02-23 23:38:42 +0300

    Added search method.

--- A-Star.py
+++ A-Star.py
@@ -35,4 +35,31 @@ def heuristic(a,b):
     return abs(x1 - x2) + abs(y1 - y2)
 
 
+def a_start_search(graph, start, goal):
+    """
+    Implementing A * search
+    """
+    frontier  = PriorityQueue()
+    frontier.put(start,0)
+    came_from = {}
+    cost_so_far = {}
 
+    came_from[start]  = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current  = frontier.get()
+
+        if current = goal:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next]  =  new_cost
+                priority =  new_cost +heuristic(goal,next)
+                 frontier.put(next, priority)
+                 came_from[next] = current
+    return came_from, cost_so_far
+
+    # TRying the code here..
