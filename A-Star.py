a8606628f408a66bbba6a0a7c952ae47ff88400d
Gabriel Oduori <gabriel.oduori@gmail.com>
2020-04-17 21:13:39 +0300

    Heuristic
    Added a heuristic function to estimate the shortest distance between two point.

--- A-Star.py
+++ A-Star.py
@@ -57,7 +57,7 @@ def a_star_search(graph, start, goal):
             new_cost = cost_so_far[current] + graph.cost(current, next)
             if next not in cost_so_far or new_cost < cost_so_far[next]:
                 cost_so_far[next]  =  new_cost
-                priority =  new_cost +heuristic(goal,next)
+                priority =  new_cost + heuristic(goal,next)
                 frontier.put(next, priority)
                 came_from[next] = current
     return came_from, cost_so_far
